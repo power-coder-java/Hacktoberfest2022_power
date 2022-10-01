a50deaf0e7dbe6f88791c0d5c423e4faad8c4a3c
Mobasshir Imam <iibrahimimam24@gmail.com>
2022-10-02 01:27:16 +0530

    Added Merge, Bubble, Quick Sort in java

---
+++ Java/QuickSort.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = { 5, 4, 3, 2, 1, 6 };
+        quicksort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            int p = partition(arr, low, high);
+            quicksort(arr, low, p - 1);
+            quicksort(arr, p + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int p = low, j;
+        for (j = low + 1; j <= high; j++)
+            if (arr[j] < arr[low])
+                swap(arr, ++p, j);
+
+        swap(arr, low, p);
+        return p;
+    }
+
+    static void swap(int[] arr, int low, int pivot) {
+        int tmp = arr[low];
+        arr[low] = arr[pivot];
+        arr[pivot] = tmp;
+    }
+}
